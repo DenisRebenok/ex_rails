d66870a29d05481b76e046a3b2f17860c62f0a8e
DenisRebenok <denis.rebenok.pro@gmail.com>
2015-08-22 20:25:09 +0300

    added simple ajax action into times view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'my_first_page#index'
+  get 'times' => 'times#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/controllers/times_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TimesController, type: :controller do
+
+end

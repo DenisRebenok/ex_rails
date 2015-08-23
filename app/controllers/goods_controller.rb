2fb9eeb808cfb6bd798d2b34accda386c9645ac4
DenisRebenok <denis.rebenok.pro@gmail.com>
2015-08-23 20:34:37 +0300

    implemented crud methods for Good model

---
+++ app/controllers/goods_controller.rb
@@ -0,0 +1,36 @@
+class GoodsController < ApplicationController
+  def index
+    @goods_count = Good.count
+    @goods = Good.all
+  end
+  def show
+    @good = Good.find(params[:id])
+  end
+
+  def new
+    @good = Good.new
+  end
+  def create
+    @good = Good.new
+    @good.code = params[:good][:code]
+    @good.name = params[:good][:name]
+    @good.save
+    redirect_to goods_path
+  end
+
+  def edit
+    @good = Good.find(params[:id])
+  end
+  def update
+    @good = Good.find(params[:id])
+    @good.code = params[:good][:code]
+    @good.name = params[:good][:name]
+    @good.save
+    redirect_to goods_path
+  end
+
+  def destroy
+    Good.find(params[:id]).destroy
+    redirect_to goods_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'my_first_page#index'
+
+  resources :goods
+
   get 'times' => 'times#index'
 
   # Example of regular route:
